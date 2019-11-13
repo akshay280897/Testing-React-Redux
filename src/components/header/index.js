967f1cb3f5ee6d8b2c95184f41ba3b3fd8152569
Akshay Potdar <Akshay.Potdar@bentley.com>
2019-11-13 18:26:11 +0530

    Added header component test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.scss'
 const Header = (props) => {
     return(
-        <header>
+        <header className="headerComponent">
             <div className="wrap">
                 Test WebSite
             </div>
