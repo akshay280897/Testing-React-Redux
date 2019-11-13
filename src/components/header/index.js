9ae3a7bd7d4ff74be9b099a64912477caca3d158
Akshay Potdar <Akshay.Potdar@bentley.com>
2019-11-13 17:50:11 +0530

    added component withour test

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss'
+const Header = (props) => {
+    return(
+        <header>
+            <div className="wrap">
+                Test WebSite
+            </div>
+        </header>
+    )
+};
+
+export default Header;
