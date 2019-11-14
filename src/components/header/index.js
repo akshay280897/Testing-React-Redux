48866d7aea4c77e9fa8417008477ae7a65fa7051
Akshay Potdar <Akshay.Potdar@bentley.com>
2019-11-14 10:34:07 +0530

    Replaced function component with class component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './styles.scss'
-const Header = (props) => {
-    return(
-        <header className="headerComponent">
+
+class Header extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const {header,desc} = this.props;
+        if(!header){
+            return null;
+        }
+
+        return(
+            <header className="headerComponent">
             <div className="wrap">
-                Test WebSite
+                {header}
             </div>
+        <p className="para">{desc}</p>
         </header>
-    )
-};
+        );
+    }
+}
+// const Header = (props) => {
+//     return(
+//         <header className="headerComponent">
+//             <div className="wrap">
+//                 Test WebSite
+//             </div>
+//             <p className="para">My First Component</p>
+//         </header>
+        
+//     )
+// };
 
 export default Header;
--- src/components/header/index.test.js
+++ src/components/header/index.test.js
@@ -4,9 +4,18 @@ import Header from './index';
 import { isTSAnyKeyword, exportAllDeclaration } from '@babel/types';
 
 describe('Header Component',()=> {
-    it('It should render without errors',() =>{
+    it('It should render without props',() =>{
         const component = shallow(<Header/>);
         const wrapper = component.find('.headerComponent');
+        expect(wrapper.length).toBe(0);
+    });
+    it('It should render with props',() =>{
+        const props = {
+            header : 'testing props',
+            desc : 'Testing component using enzyme'
+        }
+        const component = shallow(<Header {...props}/>);
+        const wrapper = component.find('.para');
         expect(wrapper.length).toBe(1);
     });
 });
